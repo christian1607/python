f02629e4534fd28ec24d7ea2ebcb8234f258e47a
christian <christian@christians-MBP.home>
2019-10-06 19:52:17 -0500

    first commit

---
+++ app.py
@@ -0,0 +1,14 @@
+print("Hello world!!!  from christian")
+print("World " * 10)
+print(10 * "World ")
+
+price = 10
+rating=5.9
+name="Christian"
+isPython=False
+print(price)
+
+
+#Recibiendo inputs
+name=input("Ingrese su nombre: ")
+print("Welcome to python "+name)
