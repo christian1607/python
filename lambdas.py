0c6f7b6492a9ee337cb0ace7707011829fb15302
christian <christian@christians-MacBook-Pro.local>
2019-10-29 20:09:23 -0500

    more lambda methods

--- lambdas.py
+++ lambdas.py
@@ -14,3 +14,27 @@ def decrement_list (*vargs):
 
 
 print(decrement_list(1,2,3))
+
+
+#all
+def is_all_strings(lst):
+    return all(type(l) == str for l in lst)
+
+
+print(is_all_strings(['2',3]))
+
+
+#sorted
+numbers = [2,3,5,1,8,3,7,9,4]
+print(numbers)
+print(sorted(numbers))
+print(sorted(numbers,reverse=True))
+print(numbers)
+
+
+def extremes(ass):
+    return  min (ass),max(ass)
+
+print(extremes([1,2,3,4,5]))
+print(extremes("alcatraz"))
+print(extremes([1,2,3,4,5]))
