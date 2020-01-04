3b730d8cda116fa8c678c8be426d7388f47e5994
christian <christian.altamirano.ayala@gmail.com>
2020-01-04 18:31:45 -0500

    query parameter http client

--- client_http/cliente_http.py
+++ client_http/cliente_http.py
@@ -1,8 +1,22 @@
 import requests
 
-response = requests.get("https://icanhazdadjoke.com/", headers={"Accept":"application/json"})
+response = requests.get("https://icanhazdadjoke.com/", headers={"Accept": "application/json"})
 print(response.text)
 
+data = response.json()
+print(f'A Joke: {data["joke"]}')
 
-#def call_ycombinator():
+# def call_ycombinator():
 #    return requests.get("https://news.ycombinator.com/")
+
+# Query param
+term = input("Type a joke term: ")
+search_response = requests.get("https://icanhazdadjoke.com/search",
+                               headers={"Accept": "application/json"},
+                               params={
+                                   "term": term
+                               })
+
+data_search = search_response.json()
+for index in range(len(data_search["results"])):
+    print(f'{ index+1 , data_search["results"][index]["joke"]}')
