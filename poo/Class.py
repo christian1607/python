6077cb2c87d31428d7fa4b98804742055def57be
christian <christian.altamirano.ayala@gmail.com>
2020-01-06 11:37:00 -0500

    herencia

--- poo/Class.py
+++ poo/Class.py
@@ -8,21 +8,33 @@ class User:
         print(cls.cant_login)
 
     def __init__(self, name, last_name=''):
-        self.nombre = name
-        self.apellido = last_name
+        self._nombre = name
+        self._apellido = last_name
         User.cant_login += 1
         print('User created')
 
     def full_name(self):
-        return f'{self.nombre } {self.apellido}'
+        return f'{self.nombre } {self._apellido}'
+
+    @property
+    def nombre(self):
+        return self._nombre
+
+    @nombre.setter
+    def nombre(self, name):
+        self._nombre = name
 
     # It is like toString java's method
     def __repr__(self):
-        return f"{self.nombre} {self.apellido}"
+        return f"{self.nombre} {self._apellido}"
 
 
 user = User("Christian", "Altamirano")
 print(user.nombre)
+
+user._nombre = "Cr7"
+print(user.nombre)
+
 print(user.full_name())
 print(User.cant_login)
 
@@ -32,3 +44,5 @@ print(User.cant_login)
 User.display_users_active()
 
 print(user)
+
+print(user2.nombre)
---
+++ poo/Inheritance.py
@@ -0,0 +1,18 @@
+class Animal:
+
+    cool = True
+
+    def make_sound(self,sound):
+        print(f"This animal says {sound}")
+
+
+class Cat(Animal):
+    pass
+
+
+a = Animal()
+a.make_sound("GUAUUU")
+
+cat1 = Cat()
+cat1.make_sound("Miauuuu")
+
